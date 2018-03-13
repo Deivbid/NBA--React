106e0a185c8433a94a40a214d81a0f79f57dfbd8
David Aparicio <davidaapariciobak@gmail.com>
2018-03-13 16:37:30 -0400

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Footer = () => {
+
+	return(
+		<footer>
+			<div className="flexbox-container">
+				<div className="logo">
+					<span></span>
+				</div> 
+			</div>
+		</footer>
+	)
+
+}
+
+export default Footer
